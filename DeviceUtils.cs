3dadd871776b7102530f152d67743593b1a23a85
F3T1W <particular0010abyss@gmail.com>
2022-01-21 17:30:39 +0300

    Добавьте файлы проекта.

---
+++ DeviceUtils.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Media.Capture.Frames;
+
+namespace MicroTestPet
+{
+    internal class DeviceUtils
+    {
+        //Find all VideoCapture device on machine
+        public static async Task<DeviceInformationCollection> GetListVideoCaptureDeviceAsync()
+        {
+            DeviceInformationCollection arg = await DeviceInformation.FindAllAsync(DeviceClass.VideoCapture);
+            return arg;
+        }
+
+        //Find All AudioCaptureDevice
+        public static async Task<DeviceInformationCollection> GetListAudioCaptureDeviceAsync()
+        {
+            DeviceInformationCollection arg = await DeviceInformation.FindAllAsync(DeviceClass.AudioCapture);
+            return arg;
+        }
+
+        //Get all device, that we can make a MediaFrame devices
+        public static async Task<IReadOnlyList<MediaFrameSourceGroup>> GetListMediaFrameSource()
+        {
+            IReadOnlyList<MediaFrameSourceGroup> arg = await MediaFrameSourceGroup.FindAllAsync();
+            return arg;
+        }
+
+        public static async Task<MediaFrameSourceGroup> GetMediaFrameSourceFromId(string device_id)
+        {
+            MediaFrameSourceGroup mediaFrameDevice = await MediaFrameSourceGroup.FromIdAsync(device_id);
+            return mediaFrameDevice;
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
+
+namespace MicroTestPet
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        readonly Microphone micro = new Microphone();
+        //private DeviceInformation current;
+        //private DeviceInformationCollection devices;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            //Record.IsEnabled = false;
+            //_ = FindDevicesAsync();
+            Record.Click += Record_Click;
+            Play.Click += Play_Click;
+        }
+
+        /*private void ListMic_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ListMic.SelectedIndex == -1)
+            {
+                Record.IsEnabled = false;
+                return;
+            }
+            current = devices[ListMic.SelectedIndex];
+            Record.IsEnabled = true;
+        }
+
+        public async Task FindDevicesAsync()
+        {
+            // Finds all video capture devices
+
+            devices = await DeviceUtils.GetListAudioCaptureDeviceAsync();
+            foreach (DeviceInformation dev in devices)
+            {
+                ListMic.Items.Add(dev.Name);
+            }
+        }*/
+
+        private void Record_Click(object sender, RoutedEventArgs e)
+        {
+            if (micro.recording)
+            {
+                micro.Stop();
+            }
+            else
+            {
+                micro.Record();
+            }
+        }
+
+        private async void Play_Click(object sende, RoutedEventArgs e)
+        {
+            await micro.Play(Dispatcher);
+        }
+    }
+}
