1e9424e9f3cc60ea6411cafbdce326b6518f0882
F3T1W <particular0010abyss@gmail.com>
2022-05-31 16:07:45 +0300

    little upd

--- AudioRecorder.cs
+++ AudioRecorder.cs
@@ -1,14 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Windows.ApplicationModel;
 using Windows.Media.Capture;
 using Windows.Media.MediaProperties;
 using Windows.Storage;
 using Windows.Storage.Streams;
-using Windows.UI.Popups;
 using Windows.UI.Xaml.Controls;
 
 namespace MicroTestPet
@@ -16,17 +11,15 @@ namespace MicroTestPet
     internal class AudioRecorder
     {
         private MediaCapture _mediaCapture;
-        private InMemoryRandomAccessStream _memoryBuffer;
+        private readonly InMemoryRandomAccessStream _memoryBuffer;
         public bool IsRecording { get; set; }
-        private const string DEFAULT_AUDIO_FILENAME = "Pisechka.mp3";
-        private string _fileName;
 
         public AudioRecorder()
         {
             IsRecording = false;
-            _fileName = "";
-            _memoryBuffer = new InMemoryRandomAccessStream(); // в него записывается аудиопоток. Когда начинаем запись, он должен быть пустой. Останавливаем - очищаем его
+            _memoryBuffer = new InMemoryRandomAccessStream();
         }
+
         public async void Record()
         {
             if (IsRecording)
@@ -36,23 +29,12 @@ namespace MicroTestPet
             }
             else
             {
-                //await Initialize();
-                //await DeleteExistingFile();
                 MediaCaptureInitializationSettings settings =
                 new MediaCaptureInitializationSettings
                 {
                     StreamingCaptureMode = StreamingCaptureMode.Audio
                 };
                 _mediaCapture = new MediaCapture();
-                /*var a = _mediaCapture.InitializeAsync(settings).AsTask();
-                Task.WaitAll(a);
-                var b = _mediaCapture.StartRecordToStreamAsync(
-                  MediaEncodingProfile.CreateMp3(AudioEncodingQuality.High), _memoryBuffer).AsTask();
-                Task.WaitAll(b);
-                if (a.Status == TaskStatus.RanToCompletion && b.Status == TaskStatus.RanToCompletion)
-                {
-                    IsRecording = true;
-                }*/
                 await _mediaCapture.InitializeAsync(settings);
                 await _mediaCapture.StartRecordToStreamAsync(
                   MediaEncodingProfile.CreateMp3(AudioEncodingQuality.High), _memoryBuffer);
@@ -69,19 +51,14 @@ namespace MicroTestPet
 
         private async void SaveAudioToFile()
         {
-            /*IRandomAccessStream audioStream = _memoryBuffer.CloneStream();
-            StorageFolder storageFolder = ApplicationData.Current.LocalCacheFolder;
-            StorageFile storageFile = await storageFolder.CreateFileAsync(
-              DEFAULT_AUDIO_FILENAME, CreationCollisionOption.GenerateUniqueName);
-            this._fileName = storageFile.Name;*/
             IRandomAccessStream audioStream = _memoryBuffer.CloneStream();
             Windows.Storage.Pickers.FileSavePicker savePicker = new Windows.Storage.Pickers.FileSavePicker
             {
-                SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.MusicLibrary
+                SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.Desktop
             };
             savePicker.FileTypeChoices.Add("Plain Text", new List<string>() { ".mp3" });
-            savePicker.SuggestedFileName = "test_audio";
-            Windows.Storage.StorageFile file = await savePicker.PickSaveFileAsync();
+            savePicker.SuggestedFileName = "Pisechka";
+            StorageFile file = await savePicker.PickSaveFileAsync();
 
             using (IRandomAccessStream fileStream =
             await file.OpenAsync(FileAccessMode.ReadWrite))
